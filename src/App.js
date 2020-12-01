2ca7a906ded53e6440bc2b164b9e61e3df9dafee
UlyPanda <ulypanda44@gmail.com>
2020-12-01 14:50:02 -0600

    actually fixed it this time

--- src/App.js
+++ src/App.js
@@ -1,15 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchPage from './Components/SearchPage';
+import NewsList from './Components/NewsList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-  
-      </header>
-    </div>
-  );
+class App extends React.Component {
+constructor(props){
+  super(props)
+  state = {
+    keywordInput: '',
+    authorInput: '',
+    dateInput: '',
+    dateInSeconds: '',
+    relatedArticles: []
+  }
+ }
+    handleChange = (event) => {
+      const dateInput = new Date(event.target.value).getTime();
+      const dateSearch = dateInput.toString().slice(0,10);
+      this.setState({
+        [event.target.name]: event.target.value,
+        dateInSeconds: dateSearch
+      })
+    }
+
+    search = (event) => {
+      event.preventDefault();
+      const keyword = this.state.keywordInput;
+      const author = this.state.authorInput;
+      const date = this.state.dateInSeconds;
+      let url = '';
+
+      if (keyword && author) {
+        url = `http://hn.algolia.com/api/v1/search?query=${keyword}&tags=story, author_${author}`;
+      } else if (keyword) {
+        url = `http://hn.algolia.com/api/v1/search?query=${keyword}&tags=story`;
+      } else if (author) {
+        `http://hn.algolia.com/api/v1/search?tags=story,author_${author}`;
+      } else if (date) {
+        url = `http://hn.algolia.com/api/v1/search_by_date?tags=comment&numericFilters=created_at_i>${date}`
+      } else {
+        alert('Please enter search criteria.');
+      }
+
+      fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({
+        relatedArticles: data.hits,
+        keywordInput: '',
+        authorInput: ''
+      }))
+      .catch(error => console.log(`Error during fetching: ${error}`))
+    }
+
+    render() {
+      return(
+        <div>
+          <header className="App-header">
+            <h1>Search for news!</h1>
+            <SearchPage
+            search={this.search}
+            handleChange={this.handleChange}
+            keywordInput={this.state.keywordInput}
+            authorInput={this.state.authorInput}
+            dateInput={this.state.dateInput}
+            />
+          </header>
+          <div className="News-layout">
+            <NewsList relatedArticles={this.state.relatedArticles} />
+          </div>
+        </div>
+      )
+    }
+
 }
-
 export default App;
--- src/Components/SearchPage.js
+++ src/Components/SearchPage.js
@@ -1,40 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import SearchBar from './SearchBar';
-import NewsList from './SearchList';
 
 const SearchPage = (props) => {
-  const [input, setInput] = useState('');
-  const [newsListDefault, setNewsListDefault] = useState();
-  const [newsList, setNewsList] = useState();
-
-  const fetchData = async () => {
-    return await fetch('http://hn.algolia.com/api/v1/items/:id')
-      .then(response => response.json())
-      .then(data => {
-         setNewsList(data) 
-         setNewsListDefault(data)
-       });}
-
-  const updateInput = async (input) => {
-     const filtered = newsListDefault.filter(news => {
-      return news.name.toLowerCase().includes(input.toLowerCase())
-     })
-     setInput(input);
-     setNewsList(filtered);
-  }
-
-  useEffect( () => {fetchData()},[]);
-	
-  return (
-    <>
-      <h1>News List</h1>
-      <SearchBar 
-       input={input} 
-       onChange={updateInput}
-      />
-      <NewsList newsList={newsList}/>
-    </>
-   );
+ return(
+    <div id="newsFeed">
+    <form onSubmit={props.search}>
+        <div className="searchInput">
+            <p>Topic:</p>
+            <input
+                name="keywordInput"
+                type="text"
+                value={props.keywordInput}
+                onChange={props.handleChange}
+                placeholder="Enter Keyword"
+            /></div>
+        <div className="searchInput">
+            <p>Author:</p>
+            <input
+                name="authorInput"
+                type="text"
+                value={props.authorInput}
+                onChange={props.handleChange}
+                placeholder="Enter Author Name"
+            />
+        </div>
+        <div className="searchInput">
+            <p>Date:</p>
+            <input
+                name="dateInput"
+                type="date"
+                value={props.dateInput}
+                onChange={props.handleChange}
+            />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+    </div>
+  )
 }
 
 export default SearchPage
